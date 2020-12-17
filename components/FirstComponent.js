6799fe8c7e44a201add64268920069ca3647809b
Anna <anna@MacBook-Pro.local>
2020-12-17 16:39:09 +0100

    .

---
+++ components/FirstComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function FirstComponent () {
+    let myName = "Ania";
+        return (
+            <view>
+                <text> Moje imię to: {myName} </text>
+            </view>
+
+        );
+}
